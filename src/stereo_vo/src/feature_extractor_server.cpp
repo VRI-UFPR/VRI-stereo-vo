8978e86797a88b8329c14e40f905bfe003d0cb79
gab <gabriel.pontarolo045@gmail.com>
2024-06-05 18:18:07 -0300

    Implement feature extractor and feature matcher.

--- src/stereo_vo/src/feature_extractor_server.cpp
+++ src/stereo_vo/src/feature_extractor_server.cpp
@@ -2,7 +2,6 @@
 
 #include "stereo_vo/srv/feature_extractor.hpp"
 #include "sensor_msgs/msg/image.hpp"
-#include "vision_msgs/detection_2d_array.hpp"
 #include "std_msgs/msg/header.hpp"
 
 #include <string>
@@ -10,10 +9,46 @@
 
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/cudafeatures2d.hpp>
 
 class FeatureExtractorServer : public rclcpp::Node
 {
+private:
+
+    rclcpp::Service<stereo_vo::srv::FeatureExtractor>::SharedPtr feature_server;
+
+    cv::Ptr<cv::cuda::ORB> orb_detector;
+
+    void feature_callback(const std::shared_ptr<stereo_vo::srv::FeatureExtractor::Request> request,
+                        std::shared_ptr<stereo_vo::srv::FeatureExtractor::Response> response)
+    {
+        RCLCPP_INFO_STREAM(this->get_logger(), 
+            "Received feature extraction request. Size: " 
+            << request->image.width << "x" << request->image.height);
+            
+        cv::Mat img = cv_bridge::toCvCopy(request->image, "mono8")->image;
+
+        // Upload to gpu
+        cv::cuda::GpuMat cuda_img;
+        cuda_img.upload(img);
+
+        cv::cuda::GpuMat cuda_keypoints;
+        cv::cuda::GpuMat cuda_descriptors;
+        this->orb_detector->detectAndComputeAsync(cuda_img, cv::cuda::GpuMat(), cuda_keypoints, cuda_descriptors);
+
+        // Download from gpu
+        cv::Mat keypoints, descriptors;
+        cuda_keypoints.download(keypoints);
+        cuda_descriptors.download(descriptors);
+
+        response->key_points = *(cv_bridge::CvImage(std_msgs::msg::Header(), "mono8", keypoints).toImageMsg());
+        response->descriptors = *(cv_bridge::CvImage(std_msgs::msg::Header(), "mono8", descriptors).toImageMsg());
+
+        RCLCPP_INFO(this->get_logger(), "Feature extraction completed.");
+    }
+
 public:
+
     FeatureExtractorServer(): Node("feature_extractor_server")
     {
         RCLCPP_INFO(this->get_logger(), "Starting feature extractor server...");
@@ -29,19 +64,9 @@ public:
         this->feature_server = this->create_service<stereo_vo::srv::FeatureExtractor>(feature_extractor_service, 
             std::bind(&FeatureExtractorServer::feature_callback, this, std::placeholders::_1, std::placeholders::_2));
 
+        this->orb_detector = cv::cuda::ORB::create();
+
         RCLCPP_INFO(this->get_logger(), "Feature extractor server started.");
-    }
-
-private:
-
-    void feature_callback(const std::shared_ptr<stereo_vo::srv::FeatureExtractor::Request> request,
-                        std::shared_ptr<stereo_vo::srv::FeatureExtractor::Response> response)
-    {
-        RCLCPP_INFO_STREAM(this->get_logger(), 
-            "Received feature extraction request. Size: " 
-            << request->image.width << "x" << request->image.height);
-            
-        response->matches = vision_msgs::Detection2DArray();
     }
 };
 
--- src/stereo_vo/src/feature_matcher_server.cpp
+++ src/stereo_vo/src/feature_matcher_server.cpp
@@ -4,12 +4,63 @@
 #include "sensor_msgs/msg/image.hpp"
 
 #include <string>
+#include <vector
 
 #include <opencv2/opencv.hpp>
 
 class FeatureMatcherServer : public rclcpp::Node
 {
+private:
+
+    rclcpp::Service<stereo_vo::srv::FeatureMatcher>::SharedPtr feature_server;
+
+    cv::Ptr<cv::cuda::DescriptorMatcher> matcher;
+
+    void feature_callback(const std::shared_ptr<stereo_vo::srv::FeatureMatcher::Request> request,
+                        std::shared_ptr<stereo_vo::srv::FeatureMatcher::Response> response)
+    {
+        RCLCPP_INFO_STREAM(this->get_logger(), 
+            "Received feature matching request. Size: " 
+            << request->current_image.width << "x" << request->current_image.height << ", " 
+            << request->previous_image.width << "x" << request->previous_image.height);
+            
+        cv::Mat current_image_desc = cv_bridge::toCvCopy(request->current_image_desc, "mono8")->image;
+        cv::Mat previous_image_desc = cv_bridge::toCvCopy(request->previous_image_desc, "mono8")->image;
+
+        // Upload to gpu
+        cv::cuda::GpuMat cuda_current_desc, cuda_previous_desc;
+        cuda_current_desc.upload(current_image_desc);
+        cuda_previous_desc.upload(previous_image_desc);
+
+        std::vector<std::vector<cv::DMatch>> matches;
+        this->matcher->knnMatch(cuda_current_desc, cuda_previous_desc, matches, 2);
+
+        // As per Lowe's ratio test
+        std::vector<cv::DMatch> good_matches;
+        for (size_t i = 0; i < matches.size(); i++)
+        {
+            if (matches[i][0].distance < 0.8 * matches[i][1].distance)
+            {
+                good_matches.push_back(matches[i][0]);
+            }
+        }
+
+        // Convert to ros message
+        std::vector<int> current_indices, previous_indices, distances;
+        for (size_t i = 0; i < good_matches.size(); i++)
+        {
+            current_indices.push_back(good_matches[i].queryIdx);
+            previous_indices.push_back(good_matches[i].trainIdx);
+            distances.push_back(good_matches[i].distance);
+        }
+
+        response->current_indices = current_indices;
+        response->previous_indices = previous_indices;
+        response->distances = distances;
+    }
+
 public:
+
     FeatureMatcherServer(): Node("feature_matcher_server")
     {
         RCLCPP_INFO(this->get_logger(), "Starting feature matcher server...");
@@ -25,23 +76,12 @@ public:
         this->feature_server = this->create_service<stereo_vo::srv::FeatureMatcher>(feature_matcher_service, 
             std::bind(&FeatureMatcherServer::feature_callback, this, std::placeholders::_1, std::placeholders::_2));
 
+        // Initialize matcher
+        this->matcher = cv::cuda::DescriptorMatcher::createBFMatcher(cv::NORM_HAMMING);
+        this->matcher->train();
+
         RCLCPP_INFO(this->get_logger(), "Feature matcher server started.");
     }
-
-private:
-
-    void feature_callback(const std::shared_ptr<stereo_vo::srv::FeatureMatcher::Request> request,
-                        std::shared_ptr<stereo_vo::srv::FeatureMatcher::Response> response)
-    {
-        RCLCPP_INFO_STREAM(this->get_logger(), 
-            "Received feature matching request. Size: " 
-            << request->current_image.width << "x" << request->current_image.height << ", " 
-            << request->previous_image.width << "x" << request->previous_image.height);
-            
-        response->matches = vision_msgs::Detection2DArray();
-    }
-    
-    rclcpp::Service<stereo_vo::srv::FeatureMatcher>::SharedPtr feature_server;
 };
 
 int main(int argc, char** argv)
