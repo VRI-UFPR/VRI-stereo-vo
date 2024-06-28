d9811c332be6ad3fb1d1fc73a013b7eeaec3d457
Gabp0 <gabriel.pontarolo045@gmail.com>
2024-06-28 17:52:33 -0300

    Change to flann matcher and add symetrycal validation

--- src/stereo_vo/src/feature_extractor_server.cpp
+++ src/stereo_vo/src/feature_extractor_server.cpp
@@ -13,6 +13,7 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/cudafeatures2d.hpp>
+#include <opencv2/features2d.hpp>
 
 class FeatureExtractorServer : public rclcpp::Node
 {
@@ -21,7 +22,7 @@ private:
     rclcpp::Service<vio_msgs::srv::FeatureExtractor>::SharedPtr feature_server;
 
     cv::Ptr<cv::cuda::ORB> orb_detector;
-    cv::Ptr<cv::cuda::DescriptorMatcher> matcher;
+    cv::Ptr<cv::DescriptorMatcher> matcher;
 
     void featureExtract(const cv::Mat &img, std::vector<cv::KeyPoint> &keypoints, cv::Mat &descriptors)
     {
@@ -37,28 +38,65 @@ private:
         cuda_descriptors.download(descriptors);
     }
 
-    void featureMatch(const cv::Mat &curr_img_desc, const cv::Mat &prev_img_desc, std::vector<cv::DMatch> &good_matches)
+    void int RobustMatcher::ratioTest(std::vector<std::vector<cv::DMatch> > &matches)
     {
-        // Upload to gpu
-        cv::cuda::GpuMat cuda_curr_desc, cuda_prev_desc;
-        cuda_curr_desc.upload(curr_img_desc);
-        cuda_prev_desc.upload(prev_img_desc);
-
-        std::vector<std::vector<cv::DMatch>> matches;
-        this->matcher->knnMatch(cuda_curr_desc, cuda_prev_desc, matches, 2);
-
         // As per Lowe's ratio test
-        for (size_t i = 0; i < matches.size(); i++)
+        for ( std::vector<std::vector<cv::DMatch> >::iterator matchIterator= matches.begin(); matchIterator!= matches.end(); ++matchIterator)
         {
-            if (matches[i][0].distance < 0.70 * matches[i][1].distance)
+            if (matchIterator->size() > 1)
             {
-                good_matches.push_back(matches[i][0]);
+                if ((*matchIterator)[0].distance / (*matchIterator)[1].distance > 0.7)
+                {
+                    matchIterator->clear(); 
+                }
+            }
+            else
+            { 
+                matchIterator->clear(); 
             }
         }
     }
 
-    void feature_callback(const std::shared_ptr<vio_msgs::srv::FeatureExtractor::Request> request,
-                        std::shared_ptr<vio_msgs::srv::FeatureExtractor::Response> response)
+    void symmetryTest(const std::vector<std::vector<cv::DMatch> >& matches1, const std::vector<std::vector<cv::DMatch> >& matches2, std::vector<cv::DMatch>& symMatches)
+    {
+    // for all matches image 1 -> image 2
+        for (std::vector<std::vector<cv::DMatch> >::const_iterator matchIterator1 = matches1.begin(); matchIterator1 != matches1.end(); ++matchIterator1)
+        {
+            if (matchIterator1->empty() || matchIterator1->size() < 2)
+                continue;
+
+            for (std::vector<std::vector<cv::DMatch> >::const_iterator matchIterator2 = matches2.begin(); matchIterator2 != matches2.end(); ++matchIterator2)
+            {
+                if (matchIterator2->empty() || matchIterator2->size() < 2)
+                    continue;
+
+                // Match symmetry test
+                if ((*matchIterator1)[0].queryIdx == (*matchIterator2)[0].trainIdx && (*matchIterator2)[0].queryIdx == (*matchIterator1)[0].trainIdx)
+                {
+                    // add symmetrical match
+                    symMatches.push_back(cv::DMatch((*matchIterator1)[0].queryIdx, (*matchIterator1)[0].trainIdx, (*matchIterator1)[0].distance));
+                    break;
+                }
+            }
+        }
+    }
+
+    void featureMatch(const cv::Mat &curr_img_desc, const cv::Mat &prev_img_desc, std::vector<cv::DMatch> &good_matches)
+    {
+        std::vector<std::vector<cv::DMatch>> matches_pc, matches_cp;
+
+        // Match features
+        this->matcher->knnMatch(prev_desc, curr_desc, matches_pc, 2);
+        this->matcher->knnMatch(curr_desc, prev_desc, matches_cp, 2);
+
+        // Remove matches per Lewe's ratio test
+        this->ratioTest(matches_pc);
+        this->ratioTest(matches_cp);
+
+        this->symetryTest(matches_pc, matches_cp, good_matches);    
+    }
+
+    void feature_callback(const std::shared_ptr<vio_msgs::srv::FeatureExtractor::Request> request,std::shared_ptr<vio_msgs::srv::FeatureExtractor::Response> response)
     {
         RCLCPP_INFO_STREAM(this->get_logger(), 
             "Received feature extraction request. Size: " 
@@ -113,7 +151,13 @@ public:
 
         // Initialize feature extractor
         this->orb_detector = cv::cuda::ORB::create();
-        this->matcher = cv::cuda::DescriptorMatcher::createBFMatcher(cv::NORM_HAMMING);
+
+        // instantiate LSH index parameters
+        cv::Ptr<cv::flann::IndexParams> indexParams = cv::makePtr<cv::flann::LshIndexParams>(6, 12, 1); 
+        // instantiate flann search parameters
+        cv::Ptr<cv::flann::SearchParams> searchParams = cv::makePtr<cv::flann::SearchParams>(50); 
+
+        this->matcher = new cv::FlannBasedMatcher(indexParams, searchParams);
         // this->matcher->train();
 
         RCLCPP_INFO(this->get_logger(), "Feature extractor server started.");
